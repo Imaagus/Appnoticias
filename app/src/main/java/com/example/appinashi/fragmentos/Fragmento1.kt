253e171449c20958ca14953beca9f5388f55a021
Santidonatoo <santinodonato1@gmail.com>
2024-10-18 20:26:25 -0300

    logica_de_prueba_fragmento1

--- app/src/main/java/com/example/appinashi/fragmentos/Fragmento1.kt
+++ app/src/main/java/com/example/appinashi/fragmentos/Fragmento1.kt
@@ -4,15 +4,25 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.example.appinashi.R
 
 class Fragmento1 : Fragment() {
+    lateinit var btnPrueba: Button
+    lateinit var tvPrueba: TextView
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragmento1, container, false)
+        val view = inflater.inflate(R.layout.fragmento1, container, false)
+        btnPrueba = view.findViewById(R.id.btnPrueba)
+        tvPrueba = view.findViewById(R.id.tvPrueba)
+        btnPrueba.setOnClickListener {
+            tvPrueba.text = "Hola esta es una prueba"
+        }
+        return view
     }
 }
